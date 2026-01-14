d9a9eea9dfcab5da0853219b4d0818376a167d0a
satyanvm <satyanvm7@gmail.com>
2026-01-14 20:04:42 +0530

    implement admin section for admins

--- src/components/common/PageMeta.tsx
+++ src/components/common/PageMeta.tsx
@@ -6,12 +6,18 @@ const PageMeta = ({
 }: {
   title: string;
   description: string;
-}) => (
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-  </Helmet>
-);
+}) => {
+  const finalTitle = title.includes("BRS Academy") ? title : `${title} | BRS Academy`;
+  
+  return (
+    <Helmet>
+      <title>{finalTitle}</title>
+      <meta name="description" content={description} />
+      <link rel="icon" type="image/png" href="/images/brs-logo/brs-flat.png" />
+      <link rel="apple-touch-icon" href="/images/brs-logo/brs-flat.png" />
+    </Helmet>
+  );
+};
 
 export const AppWrapper = ({ children }: { children: React.ReactNode }) => (
   <HelmetProvider>{children}</HelmetProvider>
